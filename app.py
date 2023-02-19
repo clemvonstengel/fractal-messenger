550acbcec521a86317566eb94a81f52a2a048b39
Clem von Stengel <clem@Clems-MacBook-Pro.local>
2023-02-19 13:38:18 +0100

    basic socketio connection

--- app.py
+++ app.py
@@ -1,7 +1,42 @@
-from flask import Flask, send_from_directory
+from flask import Flask, send_from_directory, request, redirect, session
+from flask_socketio import SocketIO, join_room
+from flask_session import Session
+
+import os
+import uuid
 
 app = Flask(__name__, static_url_path='', static_folder='frontend/build')
+app.config['SECRET_KEY'] = uuid.uuid4().hex[:32].upper() #random secret key erytime
 
-@app.errorhandler(404) #serves as catch-all, lets flask work with react-router
-def serve(e):
+if os.environ.get('SERVER_NAME'):
+    app.config['SESSION_COOKIE_DOMAIN'] = os.environ.get('SERVER_NAME')
+app.config['SESSION_TYPE']="filesystem"
+Session(app)
+
+socketio = SocketIO(app, manage_session=False, cors_allowed_origins="*") 
+
+data = [
+    {"id": 0, "type": "bark", "comp_data": {"text": "data entry numba 1"}},
+    {"id": 1, "type": "bark", "comp_data": {"text": "data entry numba 2"}},
+    {"id": 2, "type": "meristem", "comp_data": {}}
+]
+
+@app.route("/")
+def serve():
+    # session["chat_id"] = ""
     return send_from_directory(app.static_folder, "index.html")
+
+@socketio.on("connect")
+def new_connection(_):
+    # # socketio.emit("set_user", getusername() if "is_logged_in" in session else "", room=request.sid)
+    # chat_id = session["chat_id"]
+    # if(chat_id):
+    #     join_room(chat_id)
+    #     socketio.emit("set_chat_id", chat_id, room=request.sid)
+    
+    socketio.emit("load", data)
+
+
+
+
+
--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,31 +1,80 @@
+import { useState, useEffect } from "react";
+import socketIOClient from "socket.io-client";
+
 import logo from './logo.svg';
 import './App.css';
 
-function Node({i, text, metadata, selected}) {
+const base_url = "http://164.90.222.205/"
+
+const type_comps = {
+  bark: [Text],
+  meristem: [Text_Entry]
+}
+
+function Text({i, j, comp_data, command_fn}) {
+  return <div key={j}>{comp_data.text}</div>
+}
+
+function Text_Entry({i, j, command_fn}) {
+  return <div key={j}>...</div>
+}
+
+function Node({i, data, selected, command_fn}) {
   return (
-    <div key={i} className={"node " + metadata.type + "-class" + (selected ? "-selected" : "")}>
-      {text}
+    <div 
+      key={i} 
+      className={"node " + data.type + "-class" + (selected ? "-selected" : "")}
+      onClick={() => command_fn(i, "select")}
+    >
+      {type_comps[data.type].map((comp, j) => comp({
+        i: i, j: j, comp_data: data.comp_data, command_fn: command_fn,
+      }))}
     </div>
   );
 }
 
 function App() {
-  let data = [
-    {text: "this is the first message in the data", metadata: {type: "default"}},
-    {text: "this is the second message in the data", metadata: {type: "default"}},
-    {text: "...", metadata: {type: "meristem"}}
-  ]
+  const [socket, set_socket] = useState(null)
+  const [data, set_data] = useState([])
+  const [selected_i, set_selected_i] = useState(-1)
 
-  let selected_i = -1
+
+
+
+  useEffect(() => {
+    set_data([
+      {id: 0, type: "bark", comp_data: {text: "frontend placeholder 1"}},
+      {id: 1, type: "bark", comp_data: {text: "frontend placeholder 2"}},
+      {id: 2, type: "meristem", comp_data: {}}
+    ])
+    const newSocket = socketIOClient(base_url)
+    set_socket(newSocket)
+    return () => newSocket.close()
+  }, [set_socket])
+
+  useEffect(() => {
+    if (socket) {
+      socket.on("load", (new_data) => {
+        set_data(new_data) 
+      })
+    }
+  }, [socket])
+
+  let command_fn = (i, command) => {
+    if (command == "select") {
+      console.log(command)
+      set_selected_i(i)
+    }
+  }
 
   return (
     <div className="App">
       <div className="header"></div>
       {data.map((data, i) => Node({
         i: i, 
-        text: data.text, 
-        metadata: data.metadata,
-        selected: i == selected_i
+        data: data,
+        selected: i == selected_i,
+        command_fn: command_fn
       }))}
     </div>
   );
